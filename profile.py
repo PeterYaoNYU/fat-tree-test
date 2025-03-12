63d802e8c0032ac02e7a364ccdfb23ae60615c6c
Peter Yao <93311390+PeterYaoNYU@users.noreply.github.com>
2025-03-12 05:46:48 +0000

    add additional switch

--- profile.py
+++ profile.py
@@ -52,10 +52,16 @@ node_switch2 = request.RawPC('switch2')
 iface7 = node_switch2.addInterface('interface-7')
 iface8 = node_switch2.addInterface('interface-9')
 
-# Node switch3
+# Node switch4: the switch between siwthc 2 and node 3 and 4. 
+node_switch4 = request.RawPC('switch4')
+iface9 = node_switch4.addInterface('interface-3')
+iface10 = node_switch4.addInterface('interface-8')
+
+# Node switch3: the switch connection switch 2 and node 2
 node_switch3 = request.RawPC('switch3')
-iface9 = node_switch3.addInterface('interface-3')
-iface10 = node_switch3.addInterface('interface-8')
+iface11 = node_switch3.addInterface('interface-11')
+iface12 = node_switch2.addInterface('interface-12')
+
 
 # --- Define Links (L2 segments) ---
 #
@@ -74,12 +80,9 @@ link_1.addInterface(iface3)
 link_1.addInterface(iface4)
 
 # link-2: Intended to carry traffic between node-2 and switch2.
-# Although switch3's iface10 is physically on this link (to "reflect" the spine),
-# CloudLab requires an IP on every member. Therefore, we assign all members an IP
-# in a /29. Later, we force switch3 to use switch2 to reach node-2.
 link_2 = request.Link('link-2')
 link_2.Site('undefined')
-link_2.addInterface(iface2)
+link_2.addInterface(iface11)
 link_2.addInterface(iface7)
 link_2.addInterface(iface10)  # Now we assign an IP below
 
@@ -88,6 +91,12 @@ link_3 = request.Link('link-3')
 link_3.Site('undefined')
 link_3.addInterface(iface8)
 link_3.addInterface(iface6)
+
+# link-4: connects swithc 3 and node 2
+link_4 =  request.Link('link-4')
+link_4.Site('undefined')
+link_4.addInterface(iface12)
+link_4.addInterface(iface2)
 
 # --- Assign IP Addresses ---
 #
@@ -104,15 +113,20 @@ iface4.addAddress(pg.IPv4Address("10.0.1.3", "255.255.255.0"))
 # For link-2, use 10.0.2.0/29.
 # We assign:
 #   - switch2 (iface7): 10.0.2.1/29
-#   - node-2 (iface2): 10.0.2.2/29
-#   - switch3 (iface10): 10.0.2.3/29
-iface7.addAddress(pg.IPv4Address("10.0.2.1", "255.255.255.248"))
-iface2.addAddress(pg.IPv4Address("10.0.2.2", "255.255.255.248"))
-iface10.addAddress(pg.IPv4Address("10.0.2.3", "255.255.255.248"))
+#   - switch 3 (iface2): 10.0.2.2/29
+#   - switch 4 (iface10): 10.0.2.3/29
+iface7.addAddress(pg.IPv4Address("10.0.2.1", "255.255.255.0"))
+iface11.addAddress(pg.IPv4Address("10.0.2.2", "255.255.255.0"))
+iface10.addAddress(pg.IPv4Address("10.0.2.3", "255.255.255.0"))
 
-# For link-3 (10.0.3.0/24)
+# For link-3 (10.0.3.0/24): connecting switch 1 and switch 2
 iface8.addAddress(pg.IPv4Address("10.0.3.1", "255.255.255.0"))
 iface6.addAddress(pg.IPv4Address("10.0.3.2", "255.255.255.0"))
+
+
+# for link connecting switch 3 to node 2: Link-4
+iface12.addAddress(pg.IPv4Address("10.0.4.1", "255.255.255.0"))
+iface2.addAddress(pg.IPv4Address("10.0.4.2", "255.255.255.0"))
 
 # --- Add Routing Commands ---
 #
@@ -126,6 +140,9 @@ sudo sysctl -w net.ipv4.ip_forward=1
 node_switch3.addService(pg.Execute(shell="bash", command="""
 sudo sysctl -w net.ipv4.ip_forward=1
 """))
+node_switch4.addService(pg.Execute(shell="bash", command="""
+sudo sysctl -w net.ipv4.ip_forward=1
+"""))
 
 # Set default gateways for the VMs.
 node_0.addService(pg.Execute(shell="bash", command="""
@@ -133,6 +150,7 @@ sudo ip route add 10.0.0.0/24 via 10.0.0.1
 sudo ip route add 10.0.1.0/24 via 10.0.0.1
 sudo ip route add 10.0.2.0/24 via 10.0.0.1
 sudo ip route add 10.0.3.0/24 via 10.0.0.1
+sudo ip route add 10.0.0.3 via 10.0.0.1
 """))
 
 node_1.addService(pg.Execute(shell="bash", command="""
@@ -140,25 +158,30 @@ sudo ip route add 10.0.0.0/24 via 10.0.0.1
 sudo ip route add 10.0.1.0/24 via 10.0.0.1
 sudo ip route add 10.0.2.0/24 via 10.0.0.1
 sudo ip route add 10.0.3.0/24 via 10.0.0.1
+sudo ip route add 10.0.0.2 via 10.0.0.1
 """))
 
 node_2.addService(pg.Execute(shell="bash", command="""
-sudo ip route add 10.0.0.0/24 via 10.0.2.1
-sudo ip route add 10.0.1.0/24 via 10.0.2.1
-sudo ip route add 10.0.2.0/24 via 10.0.2.1
-sudo ip route add 10.0.3.0/24 via 10.0.2.1
+sudo ip route add 10.0.0.0/24 via 10.0.4.1
+sudo ip route add 10.0.1.0/24 via 10.0.4.1
+sudo ip route add 10.0.2.0/24 via 10.0.4.1
+sudo ip route add 10.0.3.0/24 via 10.0.4.1
 """))
+
 node_3.addService(pg.Execute(shell="bash", command="""
 sudo ip route add 10.0.0.0/24 via 10.0.1.1
 sudo ip route add 10.0.1.0/24 via 10.0.1.1
 sudo ip route add 10.0.2.0/24 via 10.0.1.1
 sudo ip route add 10.0.3.0/24 via 10.0.1.1
+sudo ip route add 10.0.1.3 via 10.0.1.1
 """))
+
 node_4.addService(pg.Execute(shell="bash", command="""
 sudo ip route add 10.0.0.0/24 via 10.0.1.1
 sudo ip route add 10.0.1.0/24 via 10.0.1.1
 sudo ip route add 10.0.2.0/24 via 10.0.1.1
 sudo ip route add 10.0.3.0/24 via 10.0.1.1
+sudo ip route add 10.0.1.2 via 10.0.1.1
 """))
 
 #
@@ -169,8 +192,9 @@ sudo ip route add 10.0.3.0/24 via 10.0.1.1
 # On switch1 (leaf)
 node_switch1.addService(pg.Execute(shell="bash", command="""
 # To reach node-2's network (10.0.2.0/29) via switch2 reachable on link-3:
-sudo ip route add 10.0.2.0/29 via 10.0.3.1
+sudo ip route add 10.0.2.0/24 via 10.0.3.1
 sudo ip route add 10.0.1.0/24 via 10.0.3.1
+sudo ip route add 10.0.4.0/24 via 10.0.3.1
 """))
 
 # On switch2 (middle leaf)
@@ -178,19 +202,24 @@ node_switch2.addService(pg.Execute(shell="bash", command="""
 # Route to nodes on switch1 (10.0.0.0/24) via switch1 on link-3:
 sudo ip route add 10.0.0.0/24 via 10.0.3.2
 sudo ip route add 10.0.1.0/24 via 10.0.2.3
+sudo ip route add 10.0.4.0/24 via 10.0.2.2
 """))
 
-# On switch3 (spine)
+# On switch3 (tor leaf)
 node_switch3.addService(pg.Execute(shell="bash", command="""
-# Delete the automatically installed connected route for link-2.
-# (Assuming the OS interface name for iface10 is 'interface-8'; adjust if needed.)
-# sudo ip route del 10.0.2.0/29 dev interface-8
-# Now, add a static route for node-2's network via switch2 (10.0.2.1):
-# essential to add the onlink flag here. 
-sudo ip route add 10.0.2.0/29 via 10.0.2.1 onlink
-# Also, route traffic destined for switch1's network via switch1 (reachable via link-3 through switch2):
+sudo ip route add 10.0.0.0/24 via 10.0.2.1
+sudo ip route add 10.0.1.0/24 via 10.0.2.3
+"""))
+
+# On switch4 (a combination of different switch types)
+node_switch4.addService(pg.Execute(shell="bash", command="""
+sudo ip route add 10.0.4.0/24 via 10.0.2.1
 sudo ip route add 10.0.0.0/24 via 10.0.2.1
 """))
 
 # Print the generated rspec.
 pc.printRequestRSpec(request)
+
+# this command requires the device name, should probably be run after the init. 
+# can come up with a way to get the device name from the ifconfig output to automate this process. 
+# sudo ip route add 10.0.2.0/29 via 10.0.2.1 dev *** onlink
